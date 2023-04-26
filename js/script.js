aaa0423821f58d615739a4b0f5a5ea3adc35b9c2
FrannyP71 <127154382+FrannyP71@users.noreply.github.com>
2023-04-26 10:34:09 -0400

    added fetch to JS file, started CSS

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var userInput = ''
+
+
+
+fetch('http://api.openweathermap.org/geo/1.0/direct?q='+ userInput +'&limit=5&appid=330d645ab756160ca3a15f49a493501d').then(function(response) {
+    return response.json()
+}).then(function (data) {
+
+    var latitude = data[0].lat
+    var longitude = data[0].lon
+    console.log(latitude, longitude)
+    fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude +'&lon=' + longitude +'&appid=330d645ab756160ca3a15f49a493501d&units=imperial').then(function (response){
+   return response.json()
+}).then(function (data){
+    console.log(data)
+})
+})
+
+
